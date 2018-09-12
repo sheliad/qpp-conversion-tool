5e9e005001cc291489badd2dafa9c1464dca4627
Samuel Aquino <saquino@flexion.us>
2018-09-12 15:54:05 -0500

    QPPCT-906 : Separate accept headers from version 1 and 2
    This may be reversed as we will not be supporting version 1.

--- rest-api/src/main/java/gov/cms/qpp/conversion/api/controllers/v2/CpcFileControllerV2.java
+++ rest-api/src/main/java/gov/cms/qpp/conversion/api/controllers/v2/CpcFileControllerV2.java
@@ -1,4 +1,20 @@
 package gov.cms.qpp.conversion.api.controllers.v2;
 
-public class CpcFileControllerV2 {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gov.cms.qpp.conversion.api.controllers.SkeletalCpcFileController;
+import gov.cms.qpp.conversion.api.model.Constants;
+import gov.cms.qpp.conversion.api.services.CpcFileService;
+
+/**
+ * Controller to handle cpc file data version two
+ */
+@RestController
+@RequestMapping(path = "/", headers = {"Accept=" + Constants.V2_API_ACCEPT})
+public class CpcFileControllerV2 extends SkeletalCpcFileController {
+
+	public CpcFileControllerV2(final CpcFileService cpcFileService) {
+		super(cpcFileService);
+	}
 }
